cd29bb893db7d73e32237daa3118b28e985a271c
snowfaah <snowfaah17@gmail.com>
2026-03-25 11:47:11 +0530

    added procfile and requirements for render deployment

--- app.py
+++ app.py
@@ -9,7 +9,16 @@ import os
 import logging
 from flask import Flask, send_from_directory
 from flask_cors import CORS
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
 # Configure logging
 logging.basicConfig(
     level=logging.INFO,
